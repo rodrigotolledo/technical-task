6c4c48577c35e3370eeb30e6a4aa99ada62dd0ae
Rodrigo Bonilha Tolledo <rodrigotolledo@gmail.com>
2017-03-18 18:17:44 +1100

    Refactoring ManageSettingsPage

--- pages/ManageSettingsPage.rb
+++ pages/ManageSettingsPage.rb
@@ -16,8 +16,27 @@ class ManageSettingsPage
 		sleep 3 # Replace by wait.until
 	end
 
+	def call_forwarding_status
+		@wait.until { @driver.find_element(css: '#settings_call_forwarding .setting-option-value-text').text }
+	end
+
+	def confirm_call_forwarding
+		@wait.until { @driver.find_element(link_text: 'Confirm') } # Review how reliable this one is.
+	end
+
+	def submit_button
+		@wait.until { @driver.find_element(name: 'commit') } # Refactor, this has been declared already
+	end
+
+	def wait_for_call_forward_success_modal
+		@wait.until { @driver.find_element(css: '.form_info_popup.reveal-modal.padding-none.open').displayed? }
+	end
+
+	def forward_calls_to_number
+		@wait.until { @driver.find_element(id: 'my_amaysim2_setting_call_divert_number') }
+	end
+
 	def is_call_forwarding_enabled()
-		call_forwarding_status = @wait.until { @driver.find_element(css: '#settings_call_forwarding .setting-option-value-text').text }
 		return call_forwarding_status
 	end
 
@@ -28,20 +47,18 @@ class ManageSettingsPage
 			# Using execute_script because Selenium wasn't able to click on element using default click() method
 			@driver.execute_script('document.getElementById("edit_settings_call_forwarding").click()')
 
-			confirm_call_forwarding = @wait.until { @driver.find_element(link_text: 'Confirm') } # Review how reliable this one is.
-			sleep 3 # Replace by better approach
+			sleep 7 # Replace by wait.until
 			confirm_call_forwarding.click
 
-			sleep 5 # Replace by better approach
+			sleep 5 # Replace by wait.until
 
 			# Using execute_script because Selenium wasn't able to click on element using default click() method
 			@driver.execute_script('document.getElementById("my_amaysim2_setting_call_divert_false").click()')
 
-			submit_button = @wait.until { @driver.find_element(name: 'commit') } # Refactor, this has been declared already
-			sleep 5 # Replace by better approach
+			sleep 5 # Replace by wait.until
 			submit_button.submit
 
-			@wait.until { @driver.find_element(css: '.form_info_popup.reveal-modal.padding-none.open').displayed? }
+			wait_for_call_forward_success_modal
 
 			puts ">>>>>>>>>>>> Call Forwarding is now DISABLED <<<<<<<<<<<<<<"
 		end
@@ -53,18 +70,15 @@ class ManageSettingsPage
 		# Using execute_script because Selenium wasn't able to click on element using default click() method
 		@driver.execute_script('document.getElementById("edit_settings_call_forwarding").click()')
 
-		confirm_call_forwarding = @wait.until { @driver.find_element(link_text: 'Confirm') } # Review how reliable this one is.
-		sleep 3 # Replace by better approach
+		sleep 7 # Replace by wait.until
 		confirm_call_forwarding.click
 
-		forward_calls_to_number = @wait.until { @driver.find_element(id: 'my_amaysim2_setting_call_divert_number') }
 		forward_calls_to_number.clear
 		forward_calls_to_number.send_keys "0412345678"
 
-		submit_button = @wait.until { @driver.find_element(name: 'commit') } # refactor, this has been declared already
 		submit_button.submit
 
-		@wait.until { @driver.find_element(css: '.form_info_popup.reveal-modal.padding-none.open').displayed? }
+		wait_for_call_forward_success_modal
 
 		puts ">>>>>>>>>>>> Call Forwarding is now ENABLED <<<<<<<<<<<<<<"
 	end
