4315f06038b20ca4c2d8f309b6521b2500319a5e
Rodrigo Bonilha Tolledo <rodrigotolledo@gmail.com>
2017-03-18 15:24:01 +1100

    Make tests more reliable, replacing sleeps by wait.until

--- technical-task.rb
+++ technical-task.rb
@@ -1,86 +1,86 @@
 require "selenium-webdriver"
 
-# Refactor this and move to other place
-def is_call_forwarding_enabled()
-	call_forwarding_status = $driver.find_element(css: '#settings_call_forwarding .setting-option-value-text').text
-	return call_forwarding_status
-end
-
 Selenium::WebDriver::Chrome.driver_path = "driver/chromedriver"
 
 $driver = Selenium::WebDriver.for :chrome
 
+# Implicit wait (10 seconds)
+$driver.manage.timeouts.implicit_wait = 10
+
+# Explicit wait (20 seconds)
+$wait = Selenium::WebDriver::Wait.new(timeout: 20)
+
+# Refactor this and move to other place
+def is_call_forwarding_enabled()
+	call_forwarding_status = $wait.until { $driver.find_element(css: '#settings_call_forwarding .setting-option-value-text').text }
+	return call_forwarding_status
+end
+
 $driver.navigate.to "https://www.amaysim.com.au/my-account/login"
 
-amaysim_number = $driver.find_element(id: 'mobile_number')
+amaysim_number = $wait.until { $driver.find_element(id: 'mobile_number') }
 amaysim_number.send_keys "0468827174"
 
-sleep 1
-
-amaysim_password = $driver.find_element(id: 'password')
+amaysim_password = $wait.until { $driver.find_element(id: 'password') }
 amaysim_password.send_keys "theHoff34"
 
-sleep 1
-
-submit_button = $driver.find_element(name: 'commit')
+submit_button = $wait.until { $driver.find_element(name: 'commit') }
 submit_button.submit
 
-sleep 2
+# Make sure login was successful (wait till Logout link is found)
+$wait.until { $driver.find_element(class: 'logout-link') }
 
 $driver.navigate.to "https://www.amaysim.com.au/my-account/my-amaysim/settings"
 
-sleep 3
+sleep 3 # Replace by better approach
 
 if(is_call_forwarding_enabled() == "No")
-	puts ">>>>>>>>>>>> Call Forwarding is currently DISABLED <<<<<<<<<<<<<<"
+	puts "[Call Forwarding is currently DISABLED]"
 
 	# Using execute_script because Selenium wasn't able to click on element using default click() method
 	$driver.execute_script('document.getElementById("edit_settings_call_forwarding").click()')
 
-	sleep 5
-
-	confirm_call_forwarding = $driver.find_element(link_text: 'Confirm')
+	confirm_call_forwarding = $wait.until { $driver.find_element(link_text: 'Confirm') } # Review how reliable this one is.
+	sleep 3 # Replace by better approach
 	confirm_call_forwarding.click
 
-	sleep 3
+	forward_calls_to_number = $wait.until { $driver.find_element(id: 'my_amaysim2_setting_call_divert_number') }
+	forward_calls_to_number.clear
+	forward_calls_to_number.send_keys "0412345678"
 
-	forward_calls_to_number = $driver.find_element(id: 'my_amaysim2_setting_call_divert_number')
-	forward_calls_to_number.send_keys "0431535569"
-
-	sleep 3
-
-	submit_button = $driver.find_element(name: 'commit') # refactor, this has been declared already
+	submit_button = $wait.until { $driver.find_element(name: 'commit') } # refactor, this has been declared already
 	submit_button.submit
 
-	# TODO
-	# assert status was changed
-end
+	$wait.until { $driver.find_element(css: '.form_info_popup.reveal-modal.padding-none.open').displayed? }
 
-if (is_call_forwarding_enabled == "Yes")
-	puts ">>>>>>>>>>>> Call Forwarding is currently ENABLED <<<<<<<<<<<<<<"
+	puts ">>>>>>>>>>>> Call Forwarding is now ENABLED <<<<<<<<<<<<<<"
+	# TODO
+	# assert is_call_forwarding_enabled() == "Yes"
+
+else (is_call_forwarding_enabled == "Yes")
+	puts "[Call Forwarding is currently ENABLED]"
 
 	# Using execute_script because Selenium wasn't able to click on element using default click() method
 	$driver.execute_script('document.getElementById("edit_settings_call_forwarding").click()')
 
-	sleep 8
-
-	confirm_call_forwarding = $driver.find_element(link_text: 'Confirm')
+	confirm_call_forwarding = $wait.until { $driver.find_element(link_text: 'Confirm') } # Review how reliable this one is.
+	sleep 3 # Replace by better approach
 	confirm_call_forwarding.click
 
-	sleep 5
+	sleep 5 # Replace by better approach
 
 	# Using execute_script because Selenium wasn't able to click on element using default click() method
 	$driver.execute_script('document.getElementById("my_amaysim2_setting_call_divert_false").click()')
 
-	sleep 1
-
-	submit_button = $driver.find_element(name: 'commit') # refactor, this has been declared already
+	submit_button = $wait.until { $driver.find_element(name: 'commit') } # Refactor, this has been declared already
+	sleep 5 # Replace by better approach
 	submit_button.submit
 
-	# TODO
-	# assert status was changed
-end
-	
-puts ">>>>>>>>>>>> FINISHED <<<<<<<<<<<<<<"
+	$wait.until { $driver.find_element(css: '.form_info_popup.reveal-modal.padding-none.open').displayed? }
 
-driver.quit
+	puts ">>>>>>>>>>>> Call Forwarding is now DISABLED <<<<<<<<<<<<<<"
+	# TODO
+	# assert is_call_forwarding_enabled() == "No"
+end
+
+$driver.quit
